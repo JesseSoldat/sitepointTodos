a5db8e431b643169aa6f3ee28f7ae5759aa7ca92
jesse <jsoldat@hotmail.com>
2017-06-18 14:05:08 -0400

    getAllTodos test works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { TodoDataService } from './todo-data.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
   title = 'app works!';
--- src/app/todo-data.service.spec.ts
+++ src/app/todo-data.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import { Todo } from './todo';
 import { TodoDataService } from './todo-data.service';
 
 describe('TodoDataService', () => {
@@ -12,4 +12,23 @@ describe('TodoDataService', () => {
   it('should be created', inject([TodoDataService], (service: TodoDataService) => {
     expect(service).toBeTruthy();
   }));
+
+  describe('#getAllTodos()', () => {
+    it('should return an empty array by default', inject([TodoDataService], (service: TodoDataService) => {
+      expect(service.getAllTodos()).toEqual([]);
+    }));
+
+    it('should return all todos', inject([TodoDataService], (service: TodoDataService) => {
+      let todo1 = new Todo({title: 'Hello from JLab', complete: false});
+      let todo2 = new Todo({ title: 'Bye from JLab', complete: true });
+      service.addTodo(todo1);
+      service.addTodo(todo2);
+      expect(service.getAllTodos()).toEqual([todo1, todo2]);
+    }));
+  });
+
+
+
 });
+
+
